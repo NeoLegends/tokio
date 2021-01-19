9979f1f82a65638964439279093836613d9f93a0
github-action-benchmark <github@users.noreply.github.com>
2021-01-19 12:36:56 +0000

    add rt_multi_threaded (cargo) benchmark result for 5402c948eebbb4c6c695e156d68ce495a43ea81a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611059805416,
+  "lastUpdate": 1611059815795,
   "repoUrl": "https://github.com/NeoLegends/tokio",
   "entries": {
     "sync_rwlock": [
@@ -109,6 +109,56 @@ window.BENCHMARK_DATA = {
             "name": "uncontended_concurrent_single",
             "value": 1120,
             "range": "± 52",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "rt_multi_threaded": [
+      {
+        "commit": {
+          "author": {
+            "email": "heng@cantab.net",
+            "name": "Henry Gomersall",
+            "username": "hgomersall"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5402c948eebbb4c6c695e156d68ce495a43ea81a",
+          "message": "io: clarify AsyncFd docs about changes of the inner fd (#3430)",
+          "timestamp": "2021-01-17T21:39:58+01:00",
+          "tree_id": "63ed16dcd34c11314b7bda2dcb983946b5669a1e",
+          "url": "https://github.com/NeoLegends/tokio/commit/5402c948eebbb4c6c695e156d68ce495a43ea81a"
+        },
+        "date": 1611059814753,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 201332,
+            "range": "± 15079",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 748539,
+            "range": "± 38055",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 5656360,
+            "range": "± 972193",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 20865049,
+            "range": "± 2601079",
             "unit": "ns/iter"
           }
         ]
